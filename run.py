6026912701235d14b500be23bc0d028f97537283
Hugo Rosado <hugorosol17@gmail.com>
2018-07-19 20:50:28 -0500

    run.py deleted and added again.

--- run.py
+++ run.py
@@ -1,61 +1,12 @@
-# coding=utf-8
-from datetime import timedelta
-
-from flask.app import Flask
-from flask_jwt_extended.jwt_manager import JWTManager
 from flask_migrate import Migrate
-from flask_restful import Api
 
-from db import db
-from apps.models.BaseClasses import BaseResponse
-from apps.resources.Address import Address
-from apps.resources.Appointment import AppointmentDoctor, AppointmentPatient
-from apps.resources.Device import Device
-from apps.resources.District import District, DistrictModel
-from apps.resources.Doctor import Doctor, DoctorModel
-from apps.resources.DoctorSpeciality import DoctorSpeciality
-from apps.resources.Indicator import Indicator
-from apps.resources.IndicatorEntry import IndicatorEntry
-from apps.resources.IndicatorType import IndicatorType
-from apps.resources.MedicalSpeciality import MedicalSpeciality, MedicalSpecialityModel
-from apps.resources.Membership import MembershipDoctor, MembershipPatient
-from apps.resources.Patient import Patient, PatientModel
-from apps.resources.Plan import Plan, PlanModel
-from apps.resources.Prescription import PrescriptionDoctor, PrescriptionPatient
-from apps.resources.PrescriptionType import PrescriptionType, PrescriptionTypeModel
-from apps.resources.Role import Role, RoleModel
-from apps.resources.UnitsOfMeasure import UnitOfMeasure, UnitOfMeasureModel
-from apps.resources.User import User, UserModel
-from apps.resources.Login import Login
-from config.DevelopmentEnvironment import DevelopmentEnvironment
-from config.LocalEnvironment import LocalEnvironment
+from apps.app import app
+from apps.db import db
 
-APP_CONFIG_FILE = 'config/LocalEnvironment.py'
+db.init_app(app=app)
 
-app_config = {
-    'local': LocalEnvironment,
-    'development': DevelopmentEnvironment
-}
-
-
-def configure_environment(flask_app, my_env, my_path):
-    # MySQL configurations
-    flask_app.config.from_object(my_env)
-    # flask_app.config.from_pyfile('Config.py', silent=False)
-    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + my_env.DATABASE_USER + ':' + \
-                                                  my_env.DATABASE_PASSWORD + '@' + my_env.DATABASE_HOST + '/' + \
-                                                  my_env.DATABASE_DB
-
-app = Flask(__name__, instance_relative_config=True)
-configure_environment(app, app_config['local'], APP_CONFIG_FILE)
-# JWT
-app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
-app.config['JWT_SECRET_KEY'] = 'followlife'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
-# noinspection PyTypeChecker
-api = Api(app)
-jwt = JWTManager(app)
+db.init_app(app)
+migrate = Migrate(app, db)
 
 
 def load_tables():
@@ -299,91 +250,3 @@ def load_tables():
 def create_tables():
     db.create_all(app=app)
     load_tables()
-
-
-@jwt.expired_token_loader
-def expired_token_callback():
-    return BaseResponse.unauthorized_response('Expired token.')
-
-
-@jwt.invalid_token_loader
-def invalid_token_callback(error):
-    return BaseResponse.unauthorized_response('Invalid token: ' + error)
-
-
-@jwt.unauthorized_loader
-def unauthorized_callback(error):
-    return BaseResponse.unauthorized_response(error)
-
-
-# noinspection PyTypeChecker
-api.add_resource(Address, '/api/v1/addresses',
-                 '/api/v1/addresses/',
-                 '/api/v1/addresses/<string:_id>',
-                 '/api/v1/addresses/<string:_id>/doctors')
-api.add_resource(AppointmentDoctor, '/api/v1/doctors/<string:_id>/appointments',
-                 '/api/v1/doctors/<string:_id>/appointments/',
-                 '/api/v1/doctors/<string:_id>/appointments/<string:appointment_id>')
-api.add_resource(AppointmentPatient, '/api/v1/patients/<string:_id>/appointments',
-                 '/api/v1/patients/<string:_id>/appointments/',
-                 '/api/v1/patients/<string:_id>/appointments/<string:appointment_id>')
-api.add_resource(Device, '/api/v1/devices',
-                 '/api/v1/devices/',
-                 '/api/v1/devices/<string:_id>')
-api.add_resource(District, '/api/v1/districts',
-                 '/api/v1/districts/',
-                 '/api/v1/districts/<string:_id>')
-api.add_resource(Doctor, '/api/v1/doctors',
-                 '/api/v1/doctors/',
-                 '/api/v1/doctors/<string:_id>')
-api.add_resource(DoctorSpeciality, '/api/v1/doctors/<string:_id>/doctor_specialities',
-                 '/api/v1/doctors/<string:_id>/doctor_specialities/',
-                 '/api/v1/doctors/<string:_id>/doctor_specialities/<string:doc_spec_id>')
-# api.add_resource(Indicator, '/api/v1/users', '/api/v1/users/<string:_id>')
-# api.add_resource(IndicatorEntry, '/api/v1/users', '/api/v1/users/<string:_id>')
-# api.add_resource(IndicatorType, '/api/v1/users', '/api/v1/users/<string:_id>')
-api.add_resource(MedicalSpeciality, '/api/v1/medical_specialities',
-                 '/api/v1/medical_specialities/',
-                 '/api/v1/medical_specialities/<string:_id>')
-api.add_resource(MembershipDoctor, '/api/v1/doctors/<string:doctor_id>/membership',
-                 '/api/v1/doctors/<string:doctor_id>/membership/',
-                 '/api/v1/doctors/<string:doctor_id>/patients',
-                 '/api/v1/doctors/<string:doctor_id>/patients/',
-                 '/api/v1/doctors/<string:doctor_id>/patients/<string:_id>')
-api.add_resource(MembershipPatient, '/api/v1/patients/<string:patient_id>/membership',
-                 '/api/v1/patients/<string:patient_id>/membership/',
-                 '/api/v1/patients/<string:patient_id>/doctors',
-                 '/api/v1/patients/<string:patient_id>/doctors/',
-                 '/api/v1/patients/<string:patient_id>/doctors/<string:_id>')
-api.add_resource(Patient, '/api/v1/patients',
-                 '/api/v1/patients/',
-                 '/api/v1/patients/<string:_id>')
-api.add_resource(Plan, '/api/v1/plans',
-                 '/api/v1/plans/',
-                 '/api/v1/plans/<string:_id>')
-api.add_resource(PrescriptionDoctor, '/api/v1/doctors/<string:doctor_id>/patients/<string:patient_id>/prescriptions',
-                 '/api/v1/doctors/<string:doctor_id>/patients/<string:patient_id>/prescriptions/',
-                 '/api/v1/doctors/<string:doctor_id>/patients/<string:patient_id>/prescriptions/<string:_id>')
-api.add_resource(PrescriptionPatient, '/api/v1/patients/<string:patient_id>/doctors/<string:doctor_id>/prescriptions',
-                 '/api/v1/patients/<string:patient_id>/doctors/<string:doctor_id>/prescriptions/',
-                 '/api/v1/patients/<string:patient_id>/doctors/<string:doctor_id>/prescriptions/<string:_id>')
-api.add_resource(PrescriptionType, '/api/v1/prescriptions_type',
-                 '/api/v1/prescriptions_type/',
-                 '/api/v1/prescriptions_type/<string:_id>')
-api.add_resource(Role, '/api/v1/roles',
-                 '/api/v1/roles',
-                 '/api/v1/roles/<string:_id>')
-api.add_resource(UnitOfMeasure, '/api/v1/units_of_measure',
-                 '/api/v1/units_of_measure/',
-                 '/api/v1/units_of_measure/<string:_id>')
-api.add_resource(User, '/api/v1/users',
-                 '/api/v1/users/',
-                 '/api/v1/users/<string:_id>')
-api.add_resource(Login, '/api/v1/login',
-                 '/api/v1/login/',
-                 '/api/v1/auth')
-
-if __name__ == '__main__':
-    db.init_app(app)
-    migrate = Migrate(app, db)
-    app.run(debug=app_config['local'].DEBUG)
